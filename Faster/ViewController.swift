673589c4bfb3eedf370dcc20e1002590cd96b90d
Alex <garishnavi@gmail.com>
2021-05-12 00:33:57 +0300

    Initial Commit

---
+++ Faster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Faster
+//
+//  Created by Alex Cheipesh on 12.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
